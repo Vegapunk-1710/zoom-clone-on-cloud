9173195046f26986742211b59cd1c6753ce8b99e
Rony <ronymawad@gmail.com>
2022-01-10 13:19:47 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -40,35 +40,40 @@ navigator.mediaDevices.getUserMedia({
         scrollToBottom();
     })
 
-})
-
-//Answer the call 
-peer.on('call', call => {
-    call.answer(stream);
-    const video = document.createElement('video');
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
+    //Answer the call 
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
     })
-})
-//When a new user gets connected,...
-socket.on('user-connected', (userId) => {
-    connectToNewUser(userId, stream);
-})
 
-peer.on('open', id => {
-    //Give an ID to the user when they join
-    socket.emit('join-room', ROOM_ID, id);
-})
-
-const connectToNewUser = (userId, stream) => {
-    //Prepare to give away our stream
-    const call = peer.call(userId, stream);
-    const video = document.createElement('video');
-    //Calling the new user
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream);
+    //When a new user gets connected,...
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
     })
+
+
+    peer.on('open', id => {
+        //Give an ID to the user when they join
+        socket.emit('join-room', ROOM_ID, id);
+    })
+
+    const connectToNewUser = (userId, stream) => {
+        //Prepare to give away our stream
+        const call = peer.call(userId, stream);
+        const video = document.createElement('video');
+        //Calling the new user
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
 }
+
+
+})
+
+
 
 const addVideoStream = (video, stream) => {
     //Links the stream to the <div/> int the view and starts it
